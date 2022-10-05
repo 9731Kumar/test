3adb41296235588bc5a6ca51597816633f1c4748
9731Kumar <kalyankumara2502@gmail.com>
2022-10-05 21:16:46 +0530

    changes made

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -27,40 +27,6 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Test-Demo');
   });
 
-  // it(`should star the image`,() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-   
-  //   // fixture.componentInstance.defaultStarred=false;
-    
-
-  //   // const ele=fixture.debugElement.query(By.css('.cNoStarr'));
-  //   //const ele =fixture.debugElement.query(By.css('.csStariing img'))
-  //  //const ele = fixture.debugElement.nativeElement.querySelector('img.cNoStar');
-  //   //console.log(ele);
-  //   //fixture.componentInstance.clickImg();
-  //   const ele =fixture.debugElement.queryAll(By.css('.csStariing img'));
-  //  //fixture.debugElement.query(By.css('.cNoStarr')).triggerEventHandler('click',{});
-  //  fixture.debugElement.nativeElement.querySelector('img.cNoStar').triggerEventHandler('click',{});
-  //   // fixture.componentInstance.clickImg();
-  //   fixture.detectChanges();
-  //   // const ele =fixture.debugElement.query(By.css('.csStariing img')).nativeElement;
-  // // fixture.debugElement.query(By.css('img .cNoStar')).triggerEventHandler('click',{});
-
-  //   expect(ele).toContain(fixture.debugElement.query(By.css('.cStar')).nativeElement)
-  // })
-
-  // it(`should star the image`,() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //  const ele=fixture.debugElement.queryAll(By.css('.csStariing img'))
-  //  console.log(ele);
-  // //  ele.forEach((e)=>{
-             
-  // //  })
-  //  fixture.debugElement.query(By.css('.csStariing img.cStarr')).triggerEventHandler('click',{})
-  //  fixture.detectChanges();
-  //  console.log(ele)
-  //  expect(ele).toContain(fixture.debugElement.query(By.css('.csStariing img.cNoStarr')))
-  // })
 
   it(`should star the image`,() => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -76,11 +42,4 @@ describe('AppComponent', () => {
    // })
     
   })
-
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('.content span')?.textContent).toContain('Test-Demo app is running!');
-  // });
 });
